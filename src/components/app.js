4f004a2c3f1a23b988e2bfc1f6380070363ed45b
Isak La Fleur <isak.engdahl@gmail.com>
2017-04-19 06:52:54 +0200

    initial commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+
 import BookList from '../containers/book_list';
+import BookDetail from '../containers/book-details';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-      <BookList />
+        <BookList />
+        <BookDetail />
       </div>
     );
   }
---
+++ src/reducers/reducer_active_book.js
@@ -0,0 +1,12 @@
+// state is not applicable state, only the state
+// this reducer is responsible for
+
+export default function (state = null, action) {
+  switch (action.type) {
+    case 'BOOK_SELECTED':
+      return action.payload;
+    default:
+  }
+
+  return state;
+}
